f110e2f4b9d1ab1d9d0ba0a2abf113056208365f
Muhammad Omran <use.yurMind@gmail.com>
2021-05-08 17:33:10 +0200

    fix typo in in the function defentition

--- redux/sagas/habit-saga.ts
+++ redux/sagas/habit-saga.ts
@@ -12,9 +12,12 @@ import {
   PutEffect,
 } from "redux-saga/effects";
 import { Habit } from "../../types/habit";
-import { HabitActions, HabitActionTypes } from "../reducers/habit/habit-actions";
+import {
+  HabitActions,
+  HabitActionTypes,
+} from "../reducers/habit/habit-actions";
 
-const simpleAsyncCall = async (newHabit: Habit): Promise<Habit> {
+const simpleAsyncCall = async (newHabit: Habit): Promise<Habit> => {
   return await new Promise((res, rej) => {
     setTimeout(() => {
       res(newHabit);
@@ -26,10 +29,7 @@ function* simpleAsyncHabitAction(
   action: HabitActions
 ): Generator<CallEffect<Habit> | PutEffect<HabitActions>, void, Habit> {
   try {
-    const data: Habit = yield call(
-      simpleAsyncCall,
-      action.payload as Habit
-    );
+    const data: Habit = yield call(simpleAsyncCall, action.payload as Habit);
     yield put({ type: HabitActionTypes.ADD_NEW_HABIT, payload: data });
   } catch (err) {
     yield put({ type: HabitActionTypes.SEND_ADD_NEW_HABIT_FAILED });
