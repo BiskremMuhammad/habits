782c15b9613c89232490e6cf2c4626b97d5ec13e
Muhammad Omran <use.yurMind@gmail.com>
2021-05-09 14:11:52 +0200

    load fonts into the app

--- App.tsx
+++ App.tsx
@@ -6,10 +6,9 @@
 
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-
+import * as Font from "expo-font";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { SplashScreen } from "./screens/splash";
@@ -20,8 +19,35 @@ import { createStackNavigator } from "@react-navigation/stack";
  */
 const Route = createStackNavigator();
 
+// import fonts
+let fonts = {
+  "JosefinSans-Regular": require("./assets/fonts/JosefinSans-Regular.ttf"),
+  "JosefinSans-Medium": require("./assets/fonts/JosefinSans-Medium.ttf"),
+  "JosefinSans-SemiBold": require("./assets/fonts/JosefinSans-SemiBold.ttf"),
+  "JosefinSans-Bold": require("./assets/fonts/JosefinSans-Bold.ttf"),
+  "Rubik-Light": require("./assets/fonts/Rubik-Light.ttf"),
+  "Rubik-Regular": require("./assets/fonts/Rubik-Regular.ttf"),
+  "Rubik-Medium": require("./assets/fonts/Rubik-Medium.ttf"),
+  "Rubik-SemiBold": require("./assets/fonts/Rubik-SemiBold.ttf"),
+  "Rubik-Bold": require("./assets/fonts/Rubik-Bold.ttf"),
+  "Rubik-ExtraBold": require("./assets/fonts/Rubik-ExtraBold.ttf"),
+};
+
 export default function App() {
-  return (
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync(fonts);
+      setLoading(false);
+    };
+
+    loadFonts();
+  }, []);
+
+  return loading ? (
+    <></>
+  ) : (
     <Provider store={store}>
       <NavigationContainer>
         <StatusBar style="light" />
