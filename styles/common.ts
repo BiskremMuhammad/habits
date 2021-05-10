81c70e5b85728e12727cc29063462fb990ed108d
Muhammad Omran <use.yurMind@gmail.com>
2021-05-10 13:52:16 +0200

    define some common styles

---
+++ styles/common.ts
@@ -0,0 +1,15 @@
+/**
+ * @author Muhammad Omran
+ * @date 10-05-2021
+ * @description define some common styles will be globaly available for the app
+ */
+
+import { StyleSheet } from "react-native";
+
+export const CommonStyles = StyleSheet.create({
+  textWithIcon: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
