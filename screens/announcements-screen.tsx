3d8271e221f3f4bf585fb5d42e68db101526ddac
Muhammad Omran <use.yurMind@gmail.com>
2021-06-29 14:49:26 +0200

    define dummy announcements screen

--- screens/announcements-screen.tsx
+++ screens/announcements-screen.tsx
@@ -1,3 +1,9 @@
+/**
+ * @author Muhammad Omran
+ * @date 28-06-2021
+ * @description implement the announcements screen
+ */
+
 import React from "react";
 import { View, Text } from "react-native";
 
