2b93f6e900afde2aef62ad4d8781a96bc7b50a26
Muhammad Omran <use.yurMind@gmail.com>
2021-08-25 14:03:01 +0200

    fix dashboard display just add-habit while loading

--- screens/dashboard-screen.tsx
+++ screens/dashboard-screen.tsx
@@ -55,92 +55,92 @@ export const DashboardScreen = () => {
     setRooms(rooms);
   }, [habits]);
 
-  return (
-    !!habits.length && (
-      <View style={styles.container}>
-        <Header leftAction="announcement" />
-        <TitlePanel
-          extraStyles={{ flex: 1 }}
-          icon={({ style }) => <DashboardIcon style={style} />}
-          title="My Habits"
-        />
-        <View
-          style={{
-            flex: screenHeight < 800 ? 5 : screenHeight < 846 ? 4 : 3,
-          }}
+  return habits.length ? (
+    <View />
+  ) : (
+    <View style={styles.container}>
+      <Header leftAction="announcement" />
+      <TitlePanel
+        extraStyles={{ flex: 1 }}
+        icon={({ style }) => <DashboardIcon style={style} />}
+        title="My Habits"
+      />
+      <View
+        style={{
+          flex: screenHeight < 800 ? 5 : screenHeight < 846 ? 4 : 3,
+        }}
+      >
+        <MotiView
+          style={{ flex: 1 }}
+          from={{ opacity: 0, translateX: -20 }}
+          animate={{ opacity: 1, translateX: 0 }}
+          transition={{ type: "spring", delay: 300 }}
         >
-          <MotiView
-            style={{ flex: 1 }}
-            from={{ opacity: 0, translateX: -20 }}
-            animate={{ opacity: 1, translateX: 0 }}
-            transition={{ type: "spring", delay: 300 }}
-          >
-            <Animated.FlatList
-              showsHorizontalScrollIndicator={false}
-              data={rooms}
-              keyExtractor={(item: Habit) => item.id}
-              horizontal={true}
-              bounces={false}
-              decelerationRate={Platform.OS === "ios" ? 0 : 0.98}
-              renderToHardwareTextureAndroid
-              contentContainerStyle={{ alignItems: "center" }}
-              snapToInterval={CONSTANTS.DASHBOARD_ROOM_ITEM_SIZE}
-              snapToAlignment="start"
-              onScroll={Animated.event(
-                [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-                { useNativeDriver: false }
-              )}
-              scrollEventThrottle={48}
-              renderItem={({ item, index }) => {
-                if (item.id.includes("empty")) {
-                  return (
-                    <View
-                      style={{
-                        width: CONSTANTS.DASHBOARD_ROOM_EMPTY_ITEM_SIZE,
-                      }}
-                    />
-                  );
-                }
-
-                const inputRange = [
-                  (index - 2) * CONSTANTS.DASHBOARD_ROOM_ITEM_SIZE,
-                  (index - 1) * CONSTANTS.DASHBOARD_ROOM_ITEM_SIZE,
-                  index * CONSTANTS.DASHBOARD_ROOM_ITEM_SIZE,
-                ];
-
-                const translateY = scrollX.interpolate({
-                  inputRange,
-                  outputRange: [-20, -50, -20],
-                  extrapolate: "clamp",
-                });
-
+          <Animated.FlatList
+            showsHorizontalScrollIndicator={false}
+            data={rooms}
+            keyExtractor={(item: Habit) => item.id}
+            horizontal={true}
+            bounces={false}
+            decelerationRate={Platform.OS === "ios" ? 0 : 0.98}
+            renderToHardwareTextureAndroid
+            contentContainerStyle={{ alignItems: "center" }}
+            snapToInterval={CONSTANTS.DASHBOARD_ROOM_ITEM_SIZE}
+            snapToAlignment="start"
+            onScroll={Animated.event(
+              [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+              { useNativeDriver: false }
+            )}
+            scrollEventThrottle={48}
+            renderItem={({ item, index }) => {
+              if (item.id.includes("empty")) {
                 return (
                   <View
                     style={{
-                      width: CONSTANTS.DASHBOARD_ROOM_ITEM_SIZE,
-                      paddingTop: screenHeight < 800 ? 50 : 0,
+                      width: CONSTANTS.DASHBOARD_ROOM_EMPTY_ITEM_SIZE,
+                    }}
+                  />
+                );
+              }
+
+              const inputRange = [
+                (index - 2) * CONSTANTS.DASHBOARD_ROOM_ITEM_SIZE,
+                (index - 1) * CONSTANTS.DASHBOARD_ROOM_ITEM_SIZE,
+                index * CONSTANTS.DASHBOARD_ROOM_ITEM_SIZE,
+              ];
+
+              const translateY = scrollX.interpolate({
+                inputRange,
+                outputRange: [-20, -50, -20],
+                extrapolate: "clamp",
+              });
+
+              return (
+                <View
+                  style={{
+                    width: CONSTANTS.DASHBOARD_ROOM_ITEM_SIZE,
+                    paddingTop: screenHeight < 800 ? 50 : 0,
+                  }}
+                >
+                  <Animated.View
+                    style={{
+                      marginHorizontal: CONSTANTS.DASHBOARD_ROOM_SPACING,
+                      padding: CONSTANTS.DASHBOARD_ROOM_SPACING * 2,
+                      alignItems: "center",
+                      transform: [{ translateY }],
                     }}
                   >
-                    <Animated.View
-                      style={{
-                        marginHorizontal: CONSTANTS.DASHBOARD_ROOM_SPACING,
-                        padding: CONSTANTS.DASHBOARD_ROOM_SPACING * 2,
-                        alignItems: "center",
-                        transform: [{ translateY }],
-                      }}
-                    >
-                      <DashboardRoom
-                        habit={item.id.includes("ADD_HABIT") ? undefined : item}
-                      />
-                    </Animated.View>
-                  </View>
-                );
-              }}
-            />
-          </MotiView>
-        </View>
+                    <DashboardRoom
+                      habit={item.id.includes("ADD_HABIT") ? undefined : item}
+                    />
+                  </Animated.View>
+                </View>
+              );
+            }}
+          />
+        </MotiView>
       </View>
-    )
+    </View>
   );
 };
 
